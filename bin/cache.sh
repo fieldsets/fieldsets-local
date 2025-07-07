c3545463bd804f2ad654df0674ba19926e11d851
willgladstone <will.gl@dst.one>
2025-07-07 07:11:27 -0400

    add in session functions

--- bin/cache.sh
+++ bin/cache.sh
@@ -80,6 +80,25 @@ function getFieldType {
     return @($field_type_id,$field_type)
 }
 
+
+function session_cache_connect {
+    $home_path = [System.Environment]::GetEnvironmentVariable('HOME')
+    $fieldsets_session_host = [System.Environment]::GetEnvironmentVariable('FIELDSETS_SESSION_HOST')
+    $session_port = [System.Environment]::GetEnvironmentVariable('FIELDSETS_SESSION_PORT')
+    $ssh_key_path = [System.Environment]::GetEnvironmentVariable('FIELDSETS_SESSION_KEY_PATH')
+    $session_key = [System.Environment]::GetEnvironmentVariable('FIELDSETS_SESSION_KEY')
+    if ($ssh_key_path.StartsWith('~')) {
+        $ssh_key_path = $ssh_key_path.Replace('~', "$($home_path)")
+    }
+    $session_key_path = [System.IO.Path]::GetFullPath((Join-Path -Path $ssh_key_path -ChildPath $session_key))
+
+    Enter-PSSession -HostName $fieldsets_session_host -Options @{StrictHostKeyChecking='no'} -Port $session_port -KeyFilePath $session_key_path
+}
+
+function session_cache_disconnect {    
+    Exit-PSSession
+}
+
 function session_cache_init {
     Param (
         [Parameter(Mandatory=$false)][Int]$expires_sec = 86400 #24hrs by default
@@ -100,18 +119,26 @@ function session_cache_init {
     $command = 'get fieldsets_session_cache'
     $writer.WriteLine($command)
     $writer.Flush()
+    # Wait for stream Write
+    Start-Sleep -Milliseconds 1
+
     $cache_initialized = $false
 
     while ($stream.DataAvailable) {
         $read = $stream.Read($buffer, 0, 1024)
-        $readline_value = ($encoding.GetString($buffer, 0, $read)).Trim(" ","`r","`n","`t")
-        if (
-            ("$($readline_value)".Length -gt 0 ) -and
-            (!($readline_value.StartsWith('VALUE fieldsets_session_cache'))) -and
-            ($readline_value -ne 'END')
-        ) {
-            $data_value = "$($data_value)$($readline_value)"
-        }
+        $lines = ($encoding.GetString($buffer, 0, $read)).Trim(" ","`r","`t").Split("`n")
+        foreach ($line in $lines){
+            $readline_value = ("$($line)").Trim(" ","`r","`n","`t")
+
+            #Write-Output "INIT LINE: '$($readline_value)'"
+            if (
+                ("$($readline_value)".Length -gt 0 ) -and
+                (!($readline_value.StartsWith('VALUE fieldsets_session_cache'))) -and
+                ($readline_value -ne 'END')
+            ) {
+                $data_value = "$($data_value)$($readline_value)"
+            }
+        }        
     }
 
     if ($data_value.Length -gt 0) {
@@ -120,12 +147,17 @@ function session_cache_init {
         $cache_initialized = $false
     }
 
+
     if ($false -eq $cache_initialized) {
-        $data_value = ConvertTo-Json -InputObject @{'initialized' = $true}
+        $data_value = ConvertTo-Json -InputObject @{'initialized' = $true} -Compress
         $data_value_bytes = [System.Text.Encoding]::ASCII.GetBytes($data_value)
         $command = "set fieldsets_session_cache 6 $($expires_sec) $($data_value_bytes.Length)`r`n$($data_value)`r`n"
         $writer.WriteLine($command)
         $writer.Flush()
+        # Wait for stream Write
+        Start-Sleep -Milliseconds 1
+
+
     }
     $socket.Close()
 
@@ -150,19 +182,19 @@ function session_cache_set {
     $stream = $socket.GetStream()
     $writer = New-Object System.IO.StreamWriter($stream)
 
-    $data_value = $value
     $data_type = $value.GetType().Name
     $field_type = getFieldType -data_type $data_type
-    $data_value_json = ConvertTo-Json -InputObject $data_value -Compress
-    $data_value_bytes = [System.Text.Encoding]::ASCII.GetBytes($data_value_json)
-    $command = "set $($key) $($field_type[0]) $($expires_sec) $($data_value_bytes.Length)"
-    $command_data = $data_value_json
+    $data_value = ConvertTo-Json -InputObject $value -Compress
+
+    $data_value_bytes = [System.Text.Encoding]::ASCII.GetBytes($data_value)
+    $command = "set $($key) $($field_type[0]) $($expires_sec) $($data_value_bytes.Length)`r`n$($data_value)`r`n"
     $writer.WriteLine($command)
-    $writer.WriteLine($command_data)
     $writer.Flush()
+    # Wait for stream Write
+    Start-Sleep -Milliseconds 1
 
     $socket.Close()
-    return
+    return $data_value
 }
 
 
@@ -170,6 +202,7 @@ function session_cache_get {
     Param(
         [Parameter(Mandatory=$true)][String]$key
     )
+
     $cache_host = 'localhost'
     $cache_port = 11211
     $data_value = ''
@@ -180,32 +213,41 @@ function session_cache_get {
     if ($null -eq $socket) {
         return
     }
+
+
     $stream = $socket.GetStream()
     $writer = New-Object System.IO.StreamWriter($stream)
     $command = "get $($key)"
     $writer.WriteLine($command)
     $writer.Flush()
 
-    $reader = New-Object System.IO.StreamReader($stream)
+    # Wait for stream Write
+    Start-Sleep -Milliseconds 1
+
     while ($stream.DataAvailable) {
-        $read = $reader.ReadLine()
-        $readline_value = ($encoding.GetString($buffer, 0, $read)).Trim(" ","`r","`n","`t")
-        Write-Host $readline_value
-        #if (
-        #    ("$($readline_value)".Length -gt 0 ) -and
-        #    (!($readline_value.StartsWith('VALUE fieldsets_session_cache'))) -and
-        #    ($readline_value -ne 'END')
-        #) {
-            $data_value = "$($data_value)$($readline_value)"
-        #}
+        $read = $stream.Read($buffer, 0, 1024)
+        $lines = ($encoding.GetString($buffer, 0, $read)).Trim(" ","`r","`t").Split("`n")
+        foreach ($line in $lines){
+            $readline_value = ("$($line)").Trim(" ","`r","`n","`t")
+
+            if (
+                ("$($readline_value)".Length -gt 0 ) -and
+                (!($readline_value.StartsWith("VALUE $($key)"))) -and
+                ($readline_value -ne 'END')
+            ) {
+                $data_value = "$($data_value)$($readline_value)"
+            }
+            
+        }        
     }
+
     $socket.Close()
-    if ("$($data_value)".Length -gt 0) {
-        $return_val = ConvertFrom-Json -InputObject $data_value -Compress
-        return $return_val
-    } else {
-        return
+
+    if ($data_value.Length -gt 0) {
+        return $data_value
     }
+
+    return
 }
 
 function session_cache_delete {
@@ -223,6 +265,9 @@ function session_cache_delete {
     $command = "delete $($key)"
     $writer.WriteLine($command)
     $writer.Flush()
+    # Wait for stream Write
+    Start-Sleep -Milliseconds 1
+
     $socket.Close()
     return
 }
@@ -239,12 +284,15 @@ function session_cache_flush {
     $command = "flush_all "
     $writer.WriteLine($command)
     $writer.Flush()
+    # Wait for stream Write
+    Start-Sleep -Milliseconds 1
+
     $socket.Close()
     return
 }
 
 
-$fieldsets_local_host = [System.Environment]::GetEnvironmentVariable('FIELDSETS_LOCAL_HOST')
+$fieldsets_cache_host = [System.Environment]::GetEnvironmentVariable('FIELDSETS_LOCAL_HOST')
 $home_path = [System.Environment]::GetEnvironmentVariable('HOME')
 $session_port = [System.Environment]::GetEnvironmentVariable('SSH_PORT')
 $ssh_key_path = [System.Environment]::GetEnvironmentVariable('SSH_KEY_PATH')
@@ -254,22 +302,36 @@ if ($ssh_key_path.StartsWith('~')) {
 }
 $session_key_path = [System.IO.Path]::GetFullPath((Join-Path -Path $ssh_key_path -ChildPath $session_key))
 
+Write-Output "Enteringing Session:"
 
-Enter-PSSession -HostName $fieldsets_local_host -Options @{StrictHostKeyChecking='no'} -Port $session_port -KeyFilePath $session_key_path
+Enter-PSSession -HostName $fieldsets_cache_host -Options @{StrictHostKeyChecking='no'} -Port $session_port -KeyFilePath $session_key_path
+
+Write-Output "Using PWSH Session"
+
 #session_cache_delete -key 'fieldsets_session_cache'
 
 #session_cache_flush
+Write-Output "Initializing Session Environment:"
 
-$init_check = session_cache_init
+session_cache_connect
+session_cache_init | Out-Null
+Write-Output "Session Initialized"
+Write-Output "Setting Key : mykey"
 
-#Write-Output $init_check
+$set_val = session_cache_set -key 'mykey' -value 4.3212
 
-session_cache_set -key 'mykey' -value 4.3212
-
+Write-Output "Cache Key Set"
+Write-Output $set_val
+Write-Output "Fetching Cache Key: mykey"
 $value_check = session_cache_get -key 'mykey'
-
+Write-Output "Cache Data:"
 Write-Output $value_check
 
-Exit-PSSession
+Write-Output "Fetching Cache Key: fieldsets_session_cache"
+$value_check = session_cache_get -key 'fieldsets_session_cache'
+Write-Output "Cache Data:"
+Write-Output $value_check
+
+session_cache_disconnect
 
 Exit
